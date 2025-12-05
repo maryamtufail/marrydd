8f832bb1efced9b264b37817ecc027f93c9cfb11
Maryam Tufail <maryamtufail78@gmail.com>
2025-12-05 14:47:10 +0500

    main-added

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans antialiased`}>
-        {children}
+        <main>{children}</main>
+
         <Analytics />
       </body>
     </html>
